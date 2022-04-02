d5bdaeb4368f4187e9c431a84cf4ef4455ea5bf1
mohsen0fc <eshghe_i4u@yahoo.com>
2022-04-02 11:50:13 +0430

    1

--- lib/pages/homeScreen.dart
+++ lib/pages/homeScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:untitled2/pages/add_task_screen.dart';
+import 'package:untitled2/routes/routes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,13 +9,105 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.to(AddTaskScreen());
+        },
+        child: Icon(Icons.add),
+      ),
       body: SafeArea(
-          child: Container(
+        child: Stack(alignment: Alignment.bottomCenter, children: [
+          TopSectionWidget(),
+          BottomSectionWidget(),
+        ]),
+      ),
+    );
+  }
+}
+
+class TopSectionWidget extends StatelessWidget {
+  const TopSectionWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: Get.width,
+        height: Get.height,
+        color: Colors.lightBlue,
         child: Column(
-          children: [Text('data')],
-        ),
-      )),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.more_vert),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, top: 15),
+              child: CircleAvatar(
+                radius: 25,
+                child: Icon(Icons.task),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 50, top: 16),
+                child: Text(
+                  'All',
+                  style: TextStyle(fontSize: 25),
+                )),
+            Container(
+                margin: EdgeInsets.only(left: 50, top: 5),
+                child: Text('Tasks', style: TextStyle(fontSize: 18))),
+          ],
+        ));
+  }
+}
+
+class BottomSectionWidget extends StatelessWidget {
+  const BottomSectionWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width,
+      height: Get.height * 0.69,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(35), topLeft: Radius.circular(35))),
+      child: Container(
+        margin: EdgeInsets.only(left: 40, top: 20, right: 30),
+        child: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text('Title'),
+                  subtitle: Text('subTitle'),
+                  leading: Icon(Icons.height),
+                  trailing: Checkbox(
+                    value: false,
+                    side: BorderSide(color: Colors.black, width: 2),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                    onChanged: (bool? value) {},
+                  ),
+                  onTap: () {},
+                ),
+            separatorBuilder: (context, index) =>
+                Divider(height: 1, color: Colors.black45),
+            itemCount: 10),
+      ),
     );
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
+import 'package:untitled2/pages/add_task_screen.dart';
 import 'package:untitled2/pages/homeScreen.dart';
 
 class Routes {
-  static List<GetPage> get Pages =>
-      [GetPage(name: '/homeScreen', page: () => HomeScreen())];
+  static List<GetPage> get Pages => [
+        GetPage(name: '/homeScreen', page: () => HomeScreen()),
+        GetPage(name: '/addTaskScreen', page: () => AddTaskScreen())
+      ];
 }
